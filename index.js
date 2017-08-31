1763de380c3c4da0c71e38bb679a1d6045b2d21c
John <jaboyak@gmail.com>
2017-08-31 14:46:18 -0700

    added index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = function(knex, table_one, table_two, fr_key, alias) {
+  return Promise
+    .all([
+      knex(table_one).select(),
+      knex(table_two).select()
+    ])
+    .then(data => {
+      let [ ones, twos ] = data;
+      ones.forEach(one => {
+        one[alias] = [];
+        twos.map(two => {
+          if(two[fr_key] === two.id) one[alias].push(two);
+        });
+      });
+
+      return ones;
+
+    });
+};
